ce805e8e801ed7dea1ad96ed82ccc22210fd56f8
lensonlovescode <lensonisinparis@gmail.com>
2024-05-22 13:55:40 +0300

    changes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for no in range(100):
     if no != 99:
-        print("{:d}, ".format(no), end="")
+        print("{:02d}, ".format(no), end="")
     else:
-        print("{:d}".format(no))
+        print("{:02d}".format(no))
