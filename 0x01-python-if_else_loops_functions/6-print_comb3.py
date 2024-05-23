3af86a53fff5b7bd3279f7d3f779c467e2f55e5f
lensonlovescode <lensonisinparis@gmail.com>
2024-05-23 13:45:06 +0300

    3 is upper ?!

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for no in range(10):
-    for nah in range(no +1, 10):
+    for nah in range(no + 1, 10):
         if no != nah:
             if no == 8 and nah == 9:
                 print("{:d}{:d}".format(no, nah))
