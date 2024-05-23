233d3269945abffa43c5f4910e259c111449ca87
lensonlovescode <lensonisinparis@gmail.com>
2024-05-23 13:13:32 +0300

    Brainnss

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for no in range(100):
-    if no != 99:
-        print("{:02d}, ".format(no), end="")
-    else:
-        print("{:02d}".format(no))
+for no in range(10):
+    for nah in range(no +1, 10):
+        if no != nah:
+            if no == 8 and nah == 9:
+                print("{:d}{:d}".format(no, nah))
+            else:
+                print("{:d}{:d}".format(no, nah), end=", ")
