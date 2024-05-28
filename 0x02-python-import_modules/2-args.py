bf9a2edf9c803b3a33df56c71bf8356ae247cfba
lensonlovescode <lensonisinparis@gmail.com>
2024-05-28 19:49:58 +0300

    Shell handling single quotes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    print("{} arguments".format(len(sys.argv)))
+    print("{:d} arguments".format(len(sys.argv) - 1))
     counter = 1
     for i in range(len(sys.argv) - 1):
-        print("{}: {}".format(counter, sys.argv[counter]))
+        print("{:d}: {:s}".format(counter, sys.argv[counter]))
         counter += 1
