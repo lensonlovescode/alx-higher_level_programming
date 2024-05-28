407c1d113e19cf55401884794c116b8bb8caaac1
lensonlovescode <lensonisinparis@gmail.com>
2024-05-28 20:14:19 +0300

    First name == file name

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,14 @@
 import sys
 if __name__ == '__main__':
     num_args = len(sys.argv)
-    if num_args == 0:
+    if num_args == 1:
         print("{} arguments.".format(len(sys.argv) - 1))
-    counter = 1
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv) - 1):
-        print("{:d}: {:s}".format(counter, sys.argv[counter]))
-        counter += 1
+    elif num_args == 2:
+        print("1 argument:")
+        print("{}".format(sys.argv[1]))
+    else:
+        counter = 1
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv) - 1):
+            print("{:d}: {:s}".format(counter, sys.argv[counter]))
+            counter += 1
