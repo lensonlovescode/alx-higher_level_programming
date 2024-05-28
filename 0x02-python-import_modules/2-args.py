d3263a6939dcb7c6f1a193c47b6bfeadf1a37dfa
lensonlovescode <lensonisinparis@gmail.com>
2024-05-28 19:51:36 +0300

    wrong string

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    print("{:d} arguments".format(len(sys.argv) - 1))
+    print("{:d} arguments:".format(len(sys.argv) - 1))
     counter = 1
     for i in range(len(sys.argv) - 1):
         print("{:d}: {:s}".format(counter, sys.argv[counter]))
