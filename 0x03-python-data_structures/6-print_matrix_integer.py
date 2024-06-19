cbab8b58edaff78799439d34320e0458f0d421ac
lensonlovescode <lensonisinparis@gmail.com>
2024-06-19 16:53:02 +0300

    Print Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/sr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for integer in row:
+            print(" {}".format(integer), end="")
+        print()
+        
+        
