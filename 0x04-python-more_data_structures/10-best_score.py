25a458075a3acd1f5afa1e8f610b0ca82b46dd63
lensonlovescode <lensonisinparis@gmail.com>
2024-09-18 22:27:29 +0300

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return(None)
+    best_key = None
+    best_value = 0
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_value = value
+            best_key = key
+    if best_key == None:
+        return(None)
+    else:
+        return(best_key)
+    
