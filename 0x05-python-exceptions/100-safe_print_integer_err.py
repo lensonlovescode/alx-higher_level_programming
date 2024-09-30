344aeded2b596f712cbe1b9fbe640e0bf5b2ea93
lensonlovescode <lensonisinparis@gmail.com>
2024-09-30 21:19:56 +0300

    print error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except(TypeError, ValueError):
+    except Exception as e:
+        sys.stderr.write("Exception: " + str(e) + "\n")
         return (False)
     print("")
