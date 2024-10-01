3588f28b214d76e4cbe87ba8c443a5a5ed50c45a
lensonlovescode <lensonisinparis@gmail.com>
2024-10-01 20:41:25 +0300

    new square of size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module creates a class named square that defines a square
+with: Private instance attribute: size
+      Instantiation with size (no type/value verification
+"""
+
+
+class Square:
+    """
+    This is an empty class Square that defines a square
+    """
+    def __init__(self, size):
+        self.__width = size
