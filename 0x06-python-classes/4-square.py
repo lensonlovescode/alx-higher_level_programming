39329f2510374994d326d875cccd548dc2eda4be
lensonlovescode <lensonisinparis@gmail.com>
2024-10-01 21:40:39 +0300

    getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+This module creates a class named square that defines a square
+with: Private instance attribute: size
+      Instantiation with size (no type/value verification
+"""
+
+
+class Square:
+    """
+    This is an empty class Square that defines a square
+    """
+    def __init__(self, size=0):
+        """
+        initialize the class, instantiation with
+        optional size: def __init__(self, size=0):
+
+        size must be an integer, otherwise raise
+        a TypeError exception with the message
+        size must be an integer
+
+        if size is less than 0, raise a ValueError
+        exception with the message size must be >= 0
+        """
+
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        This public instance method returns the current square area
+        """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """
+        returns the size of the square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the newsize of the square to value
+        """
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
