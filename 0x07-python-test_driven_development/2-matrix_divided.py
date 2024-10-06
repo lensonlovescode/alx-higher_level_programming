5d5d45a8cdfa0af50639ac58b8d4fad6a640d8e5
lensonlovescode <lensonisinparis@gmail.com>
2024-10-06 22:14:47 +0300

    Matrix gon make you cry

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+divides a matrix by an integer
+x must be a list of lists of integers or floats, otherwise raise a TypeError
+Each row of the matrix must be of the same size, otherwise raise a TypeError
+div must be a number (integer or float), otherwise raise a TypeError exception
+div can’t be equal to 0, otherwise raise a ZeroDivisionError exception
+"""
+
+def matrix_divided(matrix, div):
+    """
+    divides a matrix by an integer
+    x must be a list of lists of integers or floats, otherwise raise a TypeError
+    Each row of the matrix must be of the same size, otherwise raise a TypeError
+    div must be a number (integer or float), otherwise raise a TypeError exception
+    div can’t be equal to 0, otherwise raise a ZeroDivisionError exception
+    """
+    length = len(matrix[0])
+    for row in matrix:
+        if len(row) is not length:
+            raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+
+    my_matrix = [[round((element / div), 2) for element in row] for row in matrix]
+    return (my_matrix)
+
