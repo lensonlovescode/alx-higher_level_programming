ef17123ce2bb2f9f255d4b341da8beb7d25e1004
lensonlovescode <lensonisinparis@gmail.com>
2024-10-14 22:24:54 +0300

    more tests

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,11 +10,6 @@ class MyList(list):
     """
     A class MyList that inherits from list
     """
-    def __init__(self):
-        """
-        Initialization method
-        """
-        super().__init__()
 
     def print_sorted(self):
         """
