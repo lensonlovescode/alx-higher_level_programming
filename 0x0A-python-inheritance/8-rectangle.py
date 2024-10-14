3548ffc56f2274b569f7dc1f596361343593617e
lensonlovescode <lensonisinparis@gmail.com>
2024-10-15 00:41:07 +0300

    docstrings

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,10 +9,15 @@ fr0m BaseGeometry in 7-base_geometry.py
 class Rectangle(BaseGeometry):
     """
     This class contains an initializtion method
+    initiating with width and height
     """
     def __init__(self, width, height):
         """
-        The instantinuator
+        The instantinuatoir
+        Instantiation with width and height
+        width and height must be private. No getter or setter
+        width and height must be positive integers
+        validated by integer_validator
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
