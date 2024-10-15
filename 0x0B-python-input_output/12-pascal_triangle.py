00101bea528397d91b6476306a93fb9a2e77cb7d
lensonlovescode <lensonisinparis@gmail.com>
2024-10-16 01:21:27 +0300

    pascals triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns
+a list of lists of integers representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    This function returns a list of lists of integers
+    representing the Pascal’s triangle of n:
+    """
+    big_list = []
+    i = 0
+    while (i < n):
+        big_list.append(list(str(11 ** i)))
+        i += 1
+    return (big_list)
