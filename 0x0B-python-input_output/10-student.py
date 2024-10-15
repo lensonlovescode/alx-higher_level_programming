e485226839cde8abb8dd85bae11fd2e973647a5a
lensonlovescode <lensonisinparis@gmail.com>
2024-10-16 00:26:13 +0300

    nailed dictionary comprehension

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This module defines a class Student that defines a student by:
+Public instance attributes:
+1. first_name
+2. last_name
+3. age
+It also defines a public method def to_json(self, attr=""):
+that retrieves a dictionary representation of a Student instance
+with the attributes to be retrieved specified in the attr list of strings
+if not specified return all attributes
+"""
+
+
+class Student:
+    """
+    A class Student that defines a student by attributes:
+    first_name, last_name and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation method with first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+        If attrs is a list of strings:
+            only attribute names contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        """
+        if attrs == None:
+            return (self.__dict__)
+        else:
+            dic = {key: value for key, value in\
+                self.__dict__.items() if key in attrs}
+            return (dic)
