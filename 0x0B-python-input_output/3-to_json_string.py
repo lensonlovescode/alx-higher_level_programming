0e0949230656aa120e6961ba28772fbde3479baa
lensonlovescode <lensonisinparis@gmail.com>
2024-10-15 20:31:03 +0300

    JSON serialization

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""
+This module contains a function that
+returns the JSON representation of an object (string):
+"""
+
+
+def to_json_string(my_obj):
+    """
+    This function returns the JSON
+    representation of an object (string):
+    """
+    return (json.dumps(my_obj))
