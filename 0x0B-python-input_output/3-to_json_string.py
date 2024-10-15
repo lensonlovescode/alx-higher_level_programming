33a99ea0b85441fddf6851329c29224ccef2c8d9
lensonlovescode <lensonisinparis@gmail.com>
2024-10-15 20:33:42 +0300

    formating

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import json
 """
 This module contains a function that
 returns the JSON representation of an object (string):
 """
 
 
+import json
 def to_json_string(my_obj):
     """
     This function returns the JSON
