19188fef54f2131bf5b406daca2de3f7d42fedec
lensonlovescode <lensonisinparis@gmail.com>
2024-11-19 22:21:17 +0300

    exceptions

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- A script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT name, score FROM second_table WHERE name IS NOT NULL ORDER BY SCORE DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY SCORE DESC;
