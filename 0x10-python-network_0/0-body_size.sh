a6374fc2f5512aea28f3bd6507509fd0f9f20cd3
lensonlovescode <lensonisinparis@gmail.com>
2025-01-30 12:17:40 +0300

    space delimeter

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a request to that URL, and displays the size of the body of the response
-curl -s "$1" | grep -i "Content-Length" | cut -d ":" -f 2 | xargs
+curl -sI "$1" | grep -i "Content-Length" | cut -d " " -f2
