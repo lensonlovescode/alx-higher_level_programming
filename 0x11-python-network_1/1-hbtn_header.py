8a3e087c197ee0f140bd14adda068243489ef2b1
lensonlovescode <lensonisinparis@gmail.com>
2025-01-31 16:12:25 +0300

    I genuinly don't know what's wrong

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A Python script that takes in a URL, sends a request to the URL
+Contains a Python script that takes in a URL, sends a request to the URL
 and displays the value of the X-Request-Id variable found in the header
 of the response.
 """
@@ -8,5 +8,8 @@ import sys
 import urllib.request
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        request_id = response.headers.get('X-Request-Id')
+
+    print(request_id)
