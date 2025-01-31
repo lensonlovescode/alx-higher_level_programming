fd0c467c1c616e1044b4a49aef901ebaacee979d
lensonlovescode <lensonisinparis@gmail.com>
2025-01-31 20:59:33 +0300

    Requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print(f"\t- type: {type(req.text)}")
+    print(f"\t- content: {req.text}")
