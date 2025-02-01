3ec5d13cd1f13ba36712e8494eee5873470ba113
lensonlovescode <lensonisinparis@gmail.com>
2025-02-01 12:51:17 +0300

    JSON API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Contains a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if sys.argv[1]:
+        data = {'q': sys.argv[1]}
+    else:
+        data = {'q': ""}
+    req = requests.post('http://0.0.0.0:5000/search_user', data)
+
+    try:
+        dict_result = req.json()
+        if dict_result:
+            print(f"[{dict_result.get('id')}] {dict_result.get('name')}")
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
