cf29f9a7c1927b63d103e2b440828c90b3caecd7
lensonlovescode <lensonisinparis@gmail.com>
2025-02-19 21:22:00 +0300

    Mandatory tasks

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,13 @@
+$("#toggle_header").on("click", function () {
+    if ($("header").hasClass("red")) {
+        $("header").removeClass("red")
+        $("header").addClass("green")
+    }
+    else if ($("header").hasClass("green")) {
+        $("header").removeClass("green")
+        $("header").addClass("red")
+    }
+    else {
+        $("header").addClass("red")
+    }
+});
